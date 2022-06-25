edd7d9e75a51f94060a492992b193dae531c6190
Igor Koneshov <igorkoneshov@mail.ru>
2022-06-25 17:17:29 +0400

    feat: add react-router-dom and create 2 routes

---
+++ client/src/componets/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login from './Login';
+import Register from './Register';
+
+const App = () => {
+  const [isLogin, setLogin] = useState(false);
+  return (
+    <div className='page'>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/componets/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+  return <div style={{ color: 'red' }}>Login</div>;
+};
+
+export default Login;
