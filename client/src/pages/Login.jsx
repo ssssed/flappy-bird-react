1e8c571600bf4cceb57c9f52b485edb24b37859e
Igor Koneshov <igorkoneshov@mail.ru>
2022-07-23 01:30:43 +0400

    feat: add validation with Formik

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,47 +4,40 @@ import { useNavigate } from 'react-router-dom';
 import appstore from '../store/appstore';
 import '../styless/login.scss';
 import { singin } from '../utils/Auth';
+import { Formik, Field, Form } from 'formik';
 
 const Login = observer(() => {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const handleLoginChange = (e) => setLogin(e.target.value);
-  const handlePasswordChange = (e) => setPassword(e.target.value);
-
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
-    singin(login, password).then((res) => {
-      localStorage.setItem('token', res.data.token);
-      appstore.setNickName(login.split('@')[0]);
-      appstore.setUserId(res.data.userId);
-      appstore.toogleLogin(true);
-      navigate('/');
-    });
+  const initialValues = {
+    email: '',
+    password: '',
   };
 
   return (
     <div className='login'>
-      <form className='form' onSubmit={handleSubmitForm}>
-        <h2 className='form__title'>Вход</h2>
-        <input
-          type='text'
-          onChange={handleLoginChange}
-          value={login}
-          placeholder='Логин'
-          className='form__input'
-        />
-        <input
-          type='password'
-          onChange={handlePasswordChange}
-          value={password}
-          placeholder='Пароль'
-          className='form__input'
-        />
-        <button type='submit' className='form__submit'>
-          Войти
-        </button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          singin(values.email, values.password).then((res) => {
+            localStorage.setItem('token', res.data.token);
+            appstore.setNickName(values.email.split('@')[0]);
+            appstore.setUserId(res.data.userId);
+            appstore.toogleLogin(true);
+            navigate('/');
+          });
+        }}
+      >
+        <Form className='form'>
+          <div className='container'>
+            <h2 className='form__title'>Вход</h2>
+            <Field name='email' type='email' className='form__input' />
+            <Field name='password' type='password' className='form__input' />
+            <button type='submit' className='form__submit'>
+              Войти
+            </button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 });
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,50 +2,88 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../styless/login.scss';
 import { register } from '../utils/Auth';
+import { Form, Field, Formik } from 'formik';
+import appstore from '../store/appstore';
 
 const Register = () => {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const handleLoginChange = (e) => setLogin(e.target.value);
-  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const initialValues = {
+    email: '',
+    password: '',
+  };
 
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
-    localStorage.setItem('login', login.split('@')[0]);
-    register(login, password).then((res) => navigate('/login'));
+  const validate = (values) => {
+    const errors = {};
+    if (!/[A-Za-z0-9]{3,15}\@[a-z]{2,8}\.[a-z]{2,}/g.test(values.email))
+      errors.email = 'Введите коректный адрес электронной почты';
+    else if (/[0-9]{3,15}\@[a-z]{2,8}\.[a-z]{2,}/g.test(values.email))
+      errors.email = 'В почте должна быть хоть 1 буква';
+    if (!/.{5,15}$/g.test(values.password))
+      errors.password = 'Длинна пароля должна быть от 5 до 15 символов';
+    else if (!/[A-Z]{1,}/g.test(values.password))
+      errors.password = 'В пароле должна быть хотя бы одна буква';
+    else if (!/[0-9]{1,}/g.test(values.password))
+      errors.password = 'В пароле должна быть хотя бы одна цифра';
+    else if (!/[!@#$%^&*]{1,}/g.test(values.password))
+      errors.password =
+        'В пароле должны присутствовать спец символы такие как: !@#$%^&*';
+    return errors;
   };
 
   return (
     <div className='login register'>
-      <form className='form' onSubmit={handleSubmitForm}>
-        <h2 className='form__title'>Регистрация</h2>
-        <input
-          type='text'
-          onChange={handleLoginChange}
-          value={login}
-          placeholder='Логин'
-          className='form__input'
-        />
-        <input
-          type='password'
-          onChange={handlePasswordChange}
-          value={password}
-          placeholder='Пароль'
-          className='form__input'
-        />
-        <button type='submit' className='form__submit'>
-          Зарегистрироваться
-        </button>
-        <p className='form__text'>
-          Уже зарегистрированны?{' '}
-          {
-            <Link to='/login' className='form__link'>
-              Войти
-            </Link>
-          }
-        </p>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={(values) => {
+          appstore.setNickName(values.email.split('@')[0]);
+          register(values.email, values.password).then((res) =>
+            navigate('/login')
+          );
+        }}
+      >
+        {({ errors, touched, isValid, dirty }) => (
+          <Form className='form'>
+            <div className='container'>
+              <h2 className='form__title'>Регистрация</h2>
+              <Field
+                type='email'
+                className='form__input'
+                name='email'
+                required
+              />
+              <span className='form__error'>
+                {touched.email && errors.email}
+              </span>
+              <Field
+                type='password'
+                className='form__input'
+                name='password'
+                required
+              />
+              <span className='form__error'>
+                {touched.password && errors.password}
+              </span>
+              <button
+                type='submit'
+                className={`form__submit ${
+                  !isValid || !dirty ? 'form__submit_disable' : ''
+                }`}
+              >
+                Зарегистрироваться
+              </button>
+              <p className='form__text'>
+                Уже зарегистрированны?{' '}
+                {
+                  <Link to='/login' className='form__link'>
+                    Войти
+                  </Link>
+                }
+              </p>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
